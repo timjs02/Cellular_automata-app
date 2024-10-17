967800385d4c48d76023a2837dc2b194214a4de0
timjs02 <schulzketim@gmail.com>
2024-10-18 00:05:18 +0200

    added display function in main.py

--- src/main.py
+++ src/main.py
@@ -2,16 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
 import imageio
-with imageio.get_writer('/path/to/movie.gif', mode='I') as writer:
-    for filename in filenames:
-        image = imageio.imread(filename)
-        writer.append_data(image)
 
-
-
+def display():
+    with imageio.get_writer('/../ext/animation.gif', mode='I') as writer:
+        for filename in filenames:
+            image = imageio.imread(filename)
+            writer.append_data(image)
 
 if __name__ == '__main__':
-    x = np.linspace(0, 10, 100)
-    y = np.sin(x)
-    plt.plot(x, y)
-    plt.show()
+    pass
